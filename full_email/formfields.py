37847492cdaaf00a4c6bebfbbc57934aa1505b0d
Jordan Reiter (dev.editlib.org) <jordan@aace.org>
2018-05-08 18:06:57 -0400

    Later versions of Django use type="email" for the email field, which cause HTML5 validation problems. Replaced with custom widget that uses a pattern rather than type="email"

--- full_email/formfields.py
+++ full_email/formfields.py
@@ -3,5 +3,16 @@ from django import forms
 from .validators import validate_full_email
 
 
+class FullEmailInput(forms.widgets.TextInput):
+    type="text"
+
+    def __init__(self, *args, **kwargs):
+        attrs = kwargs.pop('attrs', {})
+        attrs['pattern'] = r'^.*<?[^\s@]+@(?:[-\w]+.)+\.\w+>?$'
+        kwargs['attrs'] = attrs
+        super(FullEmailInput, self).__init__(*args, **kwargs)
+
+
 class FullEmailField(forms.EmailField):
+    widget = FullEmailInput
     default_validators = [validate_full_email]
